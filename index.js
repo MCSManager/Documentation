c5c0daaebd3f754a717a2939fe579fb30f202f1f
unitwk <unitwk@outlook.com>
2023-02-26 13:48:47 +0800

    Del: default jump for chinese

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 (function (window) {
   // Main
-  if (window.navigator.language.includes("zh")) {
-    window.location.href = "/#/zh-cn/";
-  }
+  // if (window.navigator.language.includes("zh")) {
+  //   window.location.href = "/#/zh-cn/";
+  // }
 })(window);
